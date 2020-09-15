6698fa2962f73b739c05efc97445e6da1491741a
beckman <beckman.new20@gmail.com>
2020-09-15 09:49:35 +0000

    toriaezu finish

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,10 +29,10 @@ class TasksController < ApplicationController
   
   def update
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
+      flash[:success] = 'Taskは正常に更新されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'Task は更新されませんでした'
+      flash.now[:danger] = 'Taskは更新されませんでした'
       render :edit
     end
   end
