6164a84183f4406a71c612fd7f8d88db0d2c2f9f
beckman <beckman.new20@gmail.com>
2020-09-19 13:06:56 +0000

    atomousukoshi

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       redirect_to root_url
     else
       flash.now[:danger] = 'ログインに失敗しました。'
-      render :new
+      redirect_to root_url
     end
   end
 
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_to task_url
   end
   
   private
